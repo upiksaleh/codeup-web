5b6e826c26b231a6a17aad6bbb3d24eee5d34ff5
upiksaleh <upxsal@gmail.com>
2024-07-21 18:40:28 +0800

    update year

--- src/components/footer/main-footer.tsx
+++ src/components/footer/main-footer.tsx
@@ -8,7 +8,7 @@ export default function MainFooter() {
     <footer>
       <div className="base-container">
         <div className="flex justify-between">
-          <div>&copy; 2023</div>
+          <div>&copy; 2024</div>
           <div className="flex items-center gap-4 ">
             <a href="https://github.com/upiksaleh" target="_blank">
               <GithubIcon className="w-6 h-6 text-gray-500 hover:text-gray-700" />
