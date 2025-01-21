ba82fd3d5ad96fbf0ce720c7d2f777bed9310a7f
upiksaleh <upxsal@gmail.com>
2025-01-21 14:41:04 +0800

    about page url override

--- site.config.ts
+++ site.config.ts
@@ -46,10 +46,11 @@ export default siteConfig({
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: null,
-
-  pageUrlAdditions: {
+  pageUrlOverrides: {
+    '/upik': '16f8ce536d5e81a9b408eb0010c9f3cf'
   },
+
+  pageUrlAdditions: {},
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
